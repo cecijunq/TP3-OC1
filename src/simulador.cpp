dd4c02c1f11e770caeefd2888285a4e5d9a5937c
Cecilia Junqueira Vieira Machado Pereira <cecilia@MacBook-Air-de-Cecilia.local>
2024-08-12 13:56:05 -0300

    entrada tá no formato do exemplo -cecília

--- src/simulador.cpp
+++ src/simulador.cpp
@@ -5,6 +5,7 @@
 #include <deque>
 #include <fstream>
 #include <sstream>
+#include <cctype>
 
 // class Bloco {
 //     public:
@@ -13,27 +14,34 @@
 //     Bloco() { ocupado = false; }
 // };
 
-void imprime_saida(std::vector<std::deque<int> > &cache, int tam_conj, int n_linhas) {
+void imprime_saida(std::vector<std::vector<std::pair<int, int> > > &cache, int tam_conj, int n_linhas) {
     int index = 0;
-    int tam = 0;
+    // int tam = 0;
 
-    // printf("--> %d\n", n_linhas);
-    // printf("--> %d\n", tam_conj);
     printf("================\nIDX V ** ADDR **\n");
-    for(int i = 0; i < n_linhas; ++i) {
-        tam = 0;
-        for (auto it = cache[i].begin(); it != cache[i].end(); it++) {
-            printf("%03d 1 %x\n", index, *it);
+    // tam = 0;
+    for(int i = 0; i < n_linhas; i++) {
+        for(int j = 0; j < tam_conj; j++) {
+            if(cache[i][j].second == 1)
+                // printf("%03d 1 %p\n", index, cache[i][j].first);
+                printf("%03d 1 0x%08X\n", index, cache[i][j].first);
+            else
+                printf("%03d 0\n", index);
             index++;
-            tam++;
-        }
-        // printf("--> tam = %d\n", tam);
-        while(tam < tam_conj) {
-            printf("%03d 0\n", index);
-            index++;
-            tam++;
+            // tam++;
         }
     }
+        // for (auto it = cache[i].begin(); it != cache[i].end(); it++) {
+        //     printf("%03d 1 %x\n", index, *it);
+        //     index++;
+        //     tam++;
+        // }
+        // while(tam < tam_conj) {
+        //     printf("%03d 0\n", index);
+        //     index++;
+        //     tam++;
+        // }
+    
 }
 
 /*
@@ -66,8 +74,15 @@ int main(int argc, char *argv[]) {
     int index = static_cast<int>(log2(n_conjuntos_total));
     int tag = 32 - index - offset;
 
-    std::vector<std::deque<int> > cache(n_conjuntos_total);
-    // std::vector<std::deque<Bloco> > cache(n_conjuntos_total);
+    std::vector<std::deque<std::pair<int, int> > > cache_fila(n_conjuntos_total);
+    std::vector<std::vector<std::pair<int, int> > > _cache(n_conjuntos_total, std::vector<std::pair<int, int> >(tam_grupo, std::make_pair(0, 0))); // ocupado, tag
+
+    // for(int i = 0; i < n_conjuntos_total; i++) {
+    //     for(int j = 0; j < tam_grupo; j++) {
+    //         printf("[%d %d] ", _cache[i][j].first, _cache[i][j].second);
+    //     }
+    //     printf("\n");
+    // }
 
     printf("%d | %d | %d\n", tag, index, offset);
 
@@ -88,39 +103,56 @@ int main(int argc, char *argv[]) {
         ss >> number;
 
         unsigned int aux = number;
-        int tag_end = aux >> (offset + index);
+        // int tag_end = aux >> (offset + index);
+        int tag_end = aux >> (offset);
         aux = aux >> offset;
+        // aux = aux >> (offset + index);
         int mask = (1 << index) - 1;
         aux = aux & mask;
 
+        // printf("number = %x\n", number);
+        // printf("tag_end = %x\n", tag_end);
+        // printf("aux = %x\n", aux);
+
         bool found = false;
-        // Bloco novo(tag_end);
-        for (auto it = cache[aux].begin(); it != cache[aux].end(); ++it) {
-            // if (it->ocupado && it->tag == tag_end) {
-            if (*it == tag_end) {
+        for (auto it = cache_fila[aux].begin(); it != cache_fila[aux].end(); ++it) {
+            if (it->first == tag_end) {
                 hits++;
                 found = true;
                 // Apaga o elemento da sua posição origial e adiciona-o novamente na última posição (pois foi o último elemento acessado)
-                cache[aux].erase(it);
-                cache[aux].push_back(tag_end);
-                // cache[aux].push_back(novo);
+                cache_fila[aux].erase(it);
+                cache_fila[aux].push_back(std::make_pair(tag_end, cache_fila[aux].size()-1));
                 break;
             }
         }
 
-        if (!found) { // endereço não tá na cache
+        if (!found) { // endereço não tá na memória cache
+            // printf("tag_end = %x\n", tag_end);
+            int pos_cache;
+            if(cache_fila[aux].size() == 0) pos_cache = 0;
+            else pos_cache = cache_fila[aux].size();
             misses++;
-            if (cache[aux].size() == tam_grupo) {
-                cache[aux].pop_front(); // Retira o primeiro elemento inserido (que é o que tá na primeira posição)
+            if (cache_fila[aux].size() == tam_grupo) {
+                pos_cache = cache_fila[aux].front().second;
+                cache_fila[aux].pop_front(); // Retira o primeiro elemento inserido (que é o que tá na primeira posição)
             }
-            cache[aux].push_back(tag_end); // Insere um novo bloco na última posição da fila
-            // cache[aux].push_back(novo); // Insere um novo bloco na última posição da fila
+            // printf("tag_end = %x\n", tag_end);
+            cache_fila[aux].push_back(std::make_pair(tag_end, cache_fila[aux].size())); // Insere um novo bloco na última posição da fila
+            _cache[aux][pos_cache].first = tag_end;
+            _cache[aux][pos_cache].second = 1;
         }
-        imprime_saida(cache, tam_grupo, n_conjuntos_total);
+        // for(int i = 0; i < n_conjuntos_total; i++) {
+        //     for(int j = 0; j < tam_grupo; j++) {
+        //         printf("[%x %d] ", _cache[i][j].first, _cache[i][j].second);
+        //     }
+        //     printf("\n");
+        // }
+        // printf("\n\n");
+        imprime_saida(_cache, tam_grupo, n_conjuntos_total);
     }
 
     file.close();
-    printf("hits = %d\nmisses = %d\n", hits, misses);
+    printf("\n#hits = %d\n#misses = %d\n", hits, misses);
 
     return 0;
 }
